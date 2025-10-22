23b988ae795354294f538c75e001575c07ff2d8f
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2025-10-22 22:00:15 +0100

    database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { CvModule } from './cv/cv.module';
 import { SkillModule } from './skill/skill.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CvModule, SkillModule, UserModule],
+  imports: [CvModule, SkillModule, UserModule,
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'tp2_user',
+        password: 'tp2_password',
+        database: 'tp2_db',
+        autoLoadEntities: true,
+        synchronize: true
+      }
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
